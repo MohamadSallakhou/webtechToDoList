541d896285e8455a9536c44a4a890f0112def19f
MohamadSallakhou <s0592381@htw-berlin.de>
2024-12-26 20:44:52 +0100

    test backend url

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 // Setze die Basis-URL für Axios
 // axios.defaults.baseURL = 'http://localhost:8080/api'; // Ändere dies später auf die Render-Backend-URL
-axios.defaults.baseURL = 'https://webtechtodolist.onrender.com/api'; // Ändere dies später auf die Render-Backend-URL
+axios.defaults.baseURL = 'https://todolist-zdkp.onrender.com/api'; // Ändere dies später auf die Render-Backend-URL
 
 // Optional: Axios global in der App verfügbar machen
 const app = createApp(App);
